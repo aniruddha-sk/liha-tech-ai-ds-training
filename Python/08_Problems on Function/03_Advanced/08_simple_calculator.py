da46c8fdc98e93fddf74dfccdf1b1b3c9f2e72e5
Aniruddha <dairymitra.official@gmail.com>
2026-09-24 21:44:01 +0530

    Add 08_simple_calculator.py

---
+++ Python/08_Problems on Function/03_Advanced/08_simple_calculator.py
@@ -0,0 +1,13 @@
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        return a / b
+
+    return "Invalid operation"
+
+print("Result =", calculator(20, 5, "divide"))
