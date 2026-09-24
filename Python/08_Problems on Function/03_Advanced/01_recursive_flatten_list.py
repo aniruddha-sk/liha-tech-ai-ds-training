67b10fd41b77959a4668924ef3e8d77dc6b00d32
Aniruddha <dairymitra.official@gmail.com>
2026-09-24 21:43:59 +0530

    Add 01_recursive_flatten_list.py

---
+++ Python/08_Problems on Function/03_Advanced/01_recursive_flatten_list.py
@@ -0,0 +1,13 @@
+def flatten(data):
+    result = []
+
+    for item in data:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+
+    return result
+
+data = [1, [2, [3, 4], 5], [6, 7]]
+print(flatten(data))
