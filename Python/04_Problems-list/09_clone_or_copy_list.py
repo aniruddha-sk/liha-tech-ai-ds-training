a4c9aa4171a5b0099817045a38e7ab004b41c429
Aniruddha <dairymitra.official@gmail.com>
2026-09-24 20:10:30 +0530

    Add 09 clone or copy list

---
+++ Python/04_Problems-list/09_clone_or_copy_list.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40]
+
+new_list = numbers.copy()
+
+print("Original list =", numbers)
+print("Copied list =", new_list)
--- Python/04_Problems-list/9_clone_copy_list.py
+++
@@ -1,8 +0,0 @@
-numbers = [10, 20, 30, 40, 50]
-
-copy=[]
-
-for i in numbers:
-    copy.append(i)
-print("original = ",numbers)
-print("Copy = ",copy)
