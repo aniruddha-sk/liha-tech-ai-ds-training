1edaf287ff3a70d2d1a3f987eb7f2e167f291ae6
Aniruddha <dairymitra.official@gmail.com>
2026-09-24 21:44:00 +0530

    Add 05_group_anagrams.py

---
+++ Python/08_Problems on Function/03_Advanced/05_group_anagrams.py
@@ -0,0 +1,15 @@
+def group_anagrams(words):
+    groups = {}
+
+    for word in words:
+        key = "".join(sorted(word))
+
+        if key not in groups:
+            groups[key] = []
+
+        groups[key].append(word)
+
+    return list(groups.values())
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(group_anagrams(words))
