6dfe38daadb3a8c885740a614b7540be43d59305
Aniruddha <dairymitra.official@gmail.com>
2026-09-24 20:10:14 +0530

    Add 07 remove duplicates from list

---
+++ Python/04_Problems-list/07_remove_duplicates_from_list.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 2, 3, 4, 4, 5]
+
+new_list = []
+
+for number in numbers:
+    if number not in new_list:
+        new_list.append(number)
+
+print(new_list)
--- Python/04_Problems-list/7_remove_duplicates.py
+++
@@ -1,8 +0,0 @@
-numbers = [1, 2, 3, 2, 4, 1, 5, 3]
-
-l = []
-
-for i in numbers:
-    if i not in l:
-        l.append(i)
-print(l)
